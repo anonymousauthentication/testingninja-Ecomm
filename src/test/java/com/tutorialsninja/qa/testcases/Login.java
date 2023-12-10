ad196d6ca5f25a8b8f78ec5ff46d7ef33b6acc6c
anonymousauthentication <manoharkantjoshi@gmail.com>
2023-12-10 22:31:18 +0530

    FirstTestCaseValidLogin

--- src/test/java/com/tutorialsninja/qa/testcases/Login.java
+++ src/test/java/com/tutorialsninja/qa/testcases/Login.java
@@ -1,5 +1,29 @@
 package com.tutorialsninja.qa.testcases;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
 public class Login {
-//https://tutorialsninja.com/demo/
+
+	@Test
+	public void verifyLoginWithValidCredentials() {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+		driver.get("https://tutorialsninja.com/demo/");
+		driver.findElement(By.linkText("My Account")).click();
+		driver.findElement(By.linkText("Login")).click();
+		driver.findElement(By.cssSelector("input[name=\"email\"]"));
+		driver.findElement(By.xpath("//input[@name=\"password\"]"));
+		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+	}
+
 }
